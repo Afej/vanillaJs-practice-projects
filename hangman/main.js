aceae80acaea9930977507842aa672abe3202827
Afej <afekurojoshua@gmail.com>
2020-09-14 01:07:38 +0100

    07 : Hangman game

--- hangman/main.js
+++ hangman/main.js
@@ -36,7 +36,26 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log('object')
+  //display wrong letters
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+  ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+ `
+  //display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😕';
+    popup.style.display = 'flex';
+  }
 }
 
 // Show notification
@@ -74,5 +93,20 @@ window.addEventListener('keydown', (e) => {
     }
   }
 })
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  //  Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+});
 
 displayWord()
