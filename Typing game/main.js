ab40e413697de7a9523e5096d698cdc52b423eb1
Afej <afekurojoshua@gmail.com>
2020-09-19 18:05:48 +0100

    12 : Typing game initial setup

---
+++ Typing game/main.js
@@ -0,0 +1,94 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+
+//init word
+let randomWord;
+
+//init score
+let score = 0;
+
+//init time
+let time = 10;
+
+// focus on input on start
+text.focus();
+
+// start count down
+const timeInterval = setInterval(updateTime, 1000);
+
+//generate random words from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+// update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+
+    //end game
+    gameOver();
+  }
+}
+
+//Gamr over
+function gameOver() {}
+
+addWordToDOM();
+
+//Evebt listeners
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+    e.target.value = "";
+  }
+});
